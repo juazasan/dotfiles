eda4e61ddfd9a79ff7d403b0b340060e4ae125ea
Juan Carlos Zamora <juazasan@msn.com>
2019-08-20 14:45:42 +0200

    added virtualbox install

---
+++ devinfra/install-virtualbox.sh
@@ -0,0 +1,7 @@
+#!/Bin/bash
+sudo apt update
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian bionic contrib"
+sudo apt update
+sudo apt install virtualbox-6.0 -y
---
+++ k8stools/install-k3d.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget -q -O - https://raw.githubusercontent.com/rancher/k3d/master/install.sh | bash
