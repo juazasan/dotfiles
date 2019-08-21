8af276a2cf1dc5271253eddf0b649e19d5cdc0fb
Juan Carlos Zamora <juazasan@msn.com>
2019-08-21 19:49:09 +0000

    added kubectl install script

--- k8stools/install-kubectl.sh
+++ k8stools/install-kubectl.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+mkdir $HOME/tmp 
+curl -L https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl -o $HOME/tmp/kubectl
+sudo chmod +x $HOME/tmp/kubectl
+sudo mv --force $HOME/tmp/kubectl /usr/local/bin
+rm -rf $HOME/tmp/kubectl
