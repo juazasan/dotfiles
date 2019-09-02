5f0ee03f5bae6ff69ef99cbcbb8942ddc09940a1
Juan Carlos Zamora <juazasan@msn.com>
2019-09-02 20:01:10 +0200

    updated install-go.sh to latest go version (12.9)

--- devframeworks/install-go.sh
+++ devframeworks/install-go.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 mkdir $HOME/tmp
-<<<<<<< HEAD
-curl -s https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz -o $HOME/tmp/go.tar.gz
-=======
-curl -s https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz -o $HOME/tmp/go.tar.gz
->>>>>>> a51894a109bef540cc04c65af9d644c762c23f44
+curl -s https://dl.google.com/go/go1.12.9.linux-amd64.tar.gz -o $HOME/tmp/go.tar.gz
 tar -C $HOME/tmp -xf $HOME/tmp/go.tar.gz
-sudo mv $HOME/tmp/go /usr/local
+sudo rm -rf /usr/local/go
+sudo mv -f $HOME/tmp/go /usr/local
 mkdir $HOME/go
 mkdir $HOME/go/src
 mkdir $HOME/go/bin
@@ -18,8 +15,4 @@ go version
 echo 'export GOROOT=/usr/local/go' >> $HOME/.profile
 echo 'export GOPATH=$HOME/go' >> $HOME/.profile
 echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> $HOME/.profile
-<<<<<<< HEAD
 rm -f $HOME/tmp/go.tar.gz
-=======
-rm -f $HOME/tmp/go.tar.gz
->>>>>>> a51894a109bef540cc04c65af9d644c762c23f44
