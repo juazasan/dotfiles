95a0c217f241b91f0e41278c00f022b4c717ceae
Juan Carlos Zamora <jzamora@microsoft.com>
2019-09-29 16:41:53 +0200

    added Microsoft git credential manager install script

--- devinfra/install-gitcredentialmgr.sh
+++ devinfra/install-gitcredentialmgr.sh
@@ -2,3 +2,4 @@
 mkdir $HOME/git-credential-manager
 curl -sL https://github.com/microsoft/Git-Credential-Manager-for-Mac-and-Linux/releases/download/git-credential-manager-2.0.4/git-credential-manager-2.0.4.jar -o $HOME/git-credential-manager/git-credential-manager.jar
 java -jar  $HOME/git-credential-manager/git-credential-manager.jar install
+git config --global credential.canFallBackToInsecureStore true
