8800e70c0142a1555481fe765e02c7e657aba889
Juan Carlos Zamora <juazasan@msn.com>
2019-12-09 11:35:30 +0000

    upgrade vagrant to version 2.2.6

--- devinfra/install-vagrant.sh
+++ devinfra/install-vagrant.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.deb
-sudo dpkg -i vagrant_2.2.5_x86_64.deb
-rm -f vagrant_2.2.5_x86_64.deb
+wget https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.deb
+sudo dpkg -i vagrant_2.2.6_x86_64.deb
+rm -f vagrant_2.2.6_x86_64.deb
 if [[ "$(< /proc/sys/kernel/osrelease)" == *Microsoft ]]; then 
     export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1" 
     echo "export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"" >> ~/.bashrc && source ~/.bashrc
