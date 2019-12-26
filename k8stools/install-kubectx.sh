b16aaea073453f586a7da257773a8e7d4fc1e64b
Juan Carlos Zamora <juazasan>
2019-12-26 14:14:17 +0100

    update install-kubectx.sh for zsh

--- k8stools/install-kubectx.sh
+++ k8stools/install-kubectx.sh
@@ -1,10 +1,9 @@
 git clone https://github.com/ahmetb/kubectx.git ~/.kubectx
-mkdir $HOME/.bash_completion
-ln -sf ~/.kubectx/completion/kubens.bash $HOME/.bash_completion/kubens
-ln -sf ~/.kubectx/completion/kubectx.bash $HOME/.bash_completion/kubectx
-cat << FOE >> ~/.bashrc
-
-
-#kubectx and kubens
-export PATH=~/.kubectx:\$PATH
-FOE
+sudo mv ~/.kubectx/kubectx /usr/local/bin
+sudo chmod +x /usr/local/bin/kubectx
+sudo mv ~/.kubectx/kubens /usr/local/bin
+sudo chmod +x /usr/local/bin/kubens
+mkdir -p ~/.oh-my-zsh/completions
+chmod -R 755 ~/.oh-my-zsh/completions
+ln -s ~/.kubectx/completion/kubectx.zsh ~/.oh-my-zsh/completions/_kubectx.zsh
+ln -s ~/.kubectx/completion/kubens.zsh ~/.oh-my-zsh/completions/_kubens.zsh
