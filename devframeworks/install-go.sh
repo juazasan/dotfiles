532348b6eb1c939c585c90072b171f679319b48f
Juan Carlos Zamora <juazasan>
2019-12-27 14:10:29 +0100

    updated go install to v1.13.5

--- devframeworks/install-go.sh
+++ devframeworks/install-go.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
-mkdir $HOME/tmp
-curl -s https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz -o $HOME/tmp/go.tar.gz
+#!/bin/zsh
+mkdir $HOME/tmp -p
+curl -s https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz -o $HOME/tmp/go.tar.gz
 tar -C $HOME/tmp -xf $HOME/tmp/go.tar.gz
 sudo rm -rf /usr/local/go
 sudo mv -f $HOME/tmp/go /usr/local
-mkdir $HOME/go
-mkdir $HOME/go/src
-mkdir $HOME/go/bin
-mkdir $HOME/go/pkg
+mkdir $HOME/go -p
+mkdir $HOME/go/src -p
+mkdir $HOME/go/bin -p
+mkdir $HOME/go/pkg -p
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
