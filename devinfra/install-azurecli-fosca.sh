2e8d3bd123e4fe936bc542faf786eb07b1e5d806
Juan Carlos Zamora <juazasan@msn.com>
2020-08-28 12:17:34 +0200

    fixed azure-cli install scripts

--- devinfra/install-azurecli-fosca.sh
+++ devinfra/install-azurecli-fosca.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg -y
 curl -sL https://packages.microsoft.com/keys/microsoft.asc |
     gpg --dearmor |
     sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
 
-AZ_REPO=$(lsb_release -cs)
+AZ_REPO=bionic
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 
 sudo apt-get update
-sudo apt-get install azure-cli
+sudo apt-get install azure-cli -y
