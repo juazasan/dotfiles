2e83790988f504efe18fcf0a54ff3ee340676f07
Juan Carlos Zamora <juazasan@msn.com>
2020-08-28 12:39:04 +0200

    fixed python install script

--- devframeworks/install-python.sh
+++ devframeworks/install-python.sh
@@ -7,3 +7,4 @@ sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
 sudo update-alternatives --config python3
 sudo apt install python-pip -y
 sudo apt install python3-pip -y
+sudo apt install -y python3-venv
