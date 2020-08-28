80dd0d2ffa29d9dd42493d097fe7dc3c09812282
Juan Carlos Zamora <juazasan@msn.com>
2020-08-28 12:35:02 +0200

    fixed virtualbox install script

--- devinfra/install-virtualbox.sh
+++ devinfra/install-virtualbox.sh
@@ -1,7 +1,3 @@
-#!/Bin/bash
+#!/bin/bash
 sudo apt update
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian bionic contrib"
-sudo apt update
-sudo apt install virtualbox-6.0 -y
+sudo apt install virtualbox -y
