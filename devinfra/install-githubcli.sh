7e777fe98c8c0eee60047d434773470813507d98
Juan Carlos Zamora <juazasan@msn.com>
2020-09-13 11:19:02 +0200

    changed github cli install to apt

--- devinfra/install-githubcli.sh
+++ devinfra/install-githubcli.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 echo ''
 echo 'Installing the github cli'
-VERSION="0.11.1"
-mkdir -p $HOME/tmp
-curl -sL https://github.com/cli/cli/releases/download/v$VERSION/gh_${VERSION}_linux_amd64.deb -o $HOME/tmp/gh_cli_linux_amd64.deb
-sudo apt install $HOME/tmp/gh_cli_linux_amd64.deb
-rm $HOME/tmp/gh_cli_linux_amd64.deb
-echo 'github cli installed'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository -u https://cli.github.com/packages
+sudo apt update
+sudo apt install gh -y
 gh --version
