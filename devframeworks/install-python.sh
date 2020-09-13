d351502289f2454a0e940161b12d417a4a135ad2
Juan Carlos Zamora <juazasan@msn.com>
2020-09-13 11:54:58 +0200

    updated python install script

--- devframeworks/install-python.sh
+++ devframeworks/install-python.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
 sudo apt-get update
-sudo apt install python3.7 -y
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
-sudo update-alternatives --config python3
-sudo apt install python-pip -y
+#sudo apt install python3.7 -y
+#sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+#sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
+#sudo update-alternatives --config python3
+#sudo apt install python-pip -y
 sudo apt install python3-pip -y
 sudo apt install -y python3-venv
