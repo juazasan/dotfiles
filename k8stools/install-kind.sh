b9aa95d3d0e349bb2e0f71fb68750b70cd4106ed
Juan Carlos Zamora <juazasan@msn.com>
2020-11-27 12:39:47 +0100

    added kind install script

---
+++ k8stools/install-kind.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+KIND_VERSION=0.9.0
+mkdir $HOME/tmp -p
+curl -Lo $HOME/tmp/kind https://kind.sigs.k8s.io/dl/v$KIND_VERSION/kind-linux-amd64
+chmod +x $HOME/tmp/kind
+sudo mv -f $HOME/tmp/kind /usr/local/bin
+kind version
