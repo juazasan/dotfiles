bddfa2f4e8abcc5522d9ad6d7e43a4f421a67821
Juan Carlos Zamora <juazasan@msn.com>
2020-12-25 12:37:42 +0000

    updated vagrant cli version to 2.2.14

--- devinfra/install-vagrant.sh
+++ devinfra/install-vagrant.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget https://releases.hashicorp.com/vagrant/2.2.10/vagrant_2.2.10_x86_64.deb
-sudo dpkg -i vagrant_2.2.10_x86_64.deb
-rm -f vagrant_2.2.10_x86_64.deb
+wget https://releases.hashicorp.com/vagrant/2.2.14/vagrant_2.2.14_x86_64.deb
+sudo dpkg -i vagrant_2.2.14_x86_64.deb
+rm -f vagrant_2.2.14_x86_64.deb
 if [[ "$(< /proc/sys/kernel/osrelease)" == *Microsoft ]]; then 
     export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1" 
     echo "export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"" >> ~/.bashrc && source ~/.bashrc
