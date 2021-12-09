13948ccadf3521b167c26e5db99e60292f71b427
Juan Carlos Zamora <juazasan@msn.com>
2021-12-09 09:41:09 +0100

    updated git credential manager secret store to cache

--- devinfra/install-gitcredentialmgr.sh
+++ devinfra/install-gitcredentialmgr.sh
@@ -5,5 +5,5 @@ curl -sL https://github.com/GitCredentialManager/git-credential-manager/releases
 sudo dpkg -i $HOME/tmp/gcmcore-linux_amd64.deb
 git-credential-manager-core configure
 rm -f $HOME/tmp/gcmcore-linux_amd64.deb
-git config --global credential.credentialStore secretservice
+git config --global credential.credentialStore cache
 #git config --global credential.canFallBackToInsecureStore true
