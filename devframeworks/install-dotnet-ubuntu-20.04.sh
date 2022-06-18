49efeea358da842a67dd37ddd8cf3b0deb353bc8
Juan Carlos Zamora <juazasan@msn.com>
2022-06-18 20:31:19 +0200

    update dotnet core install for ubuntu 20.04 to version 6

--- devframeworks/install-dotnet-ubuntu-20.04.sh
+++ devframeworks/install-dotnet-ubuntu-20.04.sh
@@ -4,4 +4,4 @@ rm $HOME/tmp/packages-microsoft-prod.deb
 sudo apt-get update; \
   sudo apt-get install -y apt-transport-https && \
   sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-5.0
+  sudo apt-get install -y dotnet-sdk-6.0
