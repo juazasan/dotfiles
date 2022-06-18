dc93312d608cf89235e0d21588114373487adda8
Juan Carlos Zamora <juazasan@msn.com>
2022-06-18 08:46:51 +0200

    added stable repo to helm install

--- k8stools/install-helm.sh
+++ k8stools/install-helm.sh
@@ -3,5 +3,6 @@ curl -L https://get.helm.sh/helm-v$helm_version-linux-amd64.tar.gz -o $HOME/tmp/
 tar zxvf $HOME/tmp/helm.tar.gz -C $HOME/tmp
 sudo mv $HOME/tmp/linux-amd64/helm /usr/local/bin
 sudo chmod +x /usr/local/bin/helm
+helm repo add stable https://charts.helm.sh/stable
 rm -rf $HOME/linux-amd64
 rm -f $HOME/tmp/helm.tar.gz
