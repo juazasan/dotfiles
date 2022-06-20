e0c1f1568f8c37f4dde407d5ea5652ffab133186
Juan Carlos Zamora <juazasan@msn.com>
2022-06-20 14:18:22 +0200

    updated docker install script

--- devinfra/install-docker.sh
+++ devinfra/install-docker.sh
@@ -5,30 +5,25 @@ sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 
 # Download and add Docker's official public PGP key.
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-# Verify the fingerprint.
-sudo apt-key fingerprint 0EBFCD88
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 # Add the `stable` channel's Docker upstream repository.
 #
 # If you want to live on the edge, you can change "stable" below to "test" or
 # "nightly". I highly recommend sticking with stable!
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update the apt package list (for the new apt repo).
 sudo apt-get update
 
 # Install the latest version of Docker CE.
-sudo apt-get install docker-ce -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 # Allow your user to access the Docker CLI without needing root access.
 sudo usermod -aG docker $USER
 newgrp docker
 
-# Install docker-compose
-sudo $(which pip3) install --user docker-compose
 echo "export PATH=~/.local/bin:$PATH" >> ~/.zprofile && source ~/.zprofile
