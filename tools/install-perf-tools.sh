b9ed1f5a581e956d1b57d5047bb45ff7f6eb71e6
Juan Carlos Zamora <juazasan@msn.com>
2022-10-28 11:18:36 +0200

    added perftools

---
+++ tools/install-perf-tools.sh
@@ -0,0 +1,2 @@
+sudo apt install procps util-linux sysstat iproute2 numactl linux-tools-common linux-tools-$(uname -r) bpfcc-tools bpftrace perf-tools-unstable trace-cmd nicstat ethtool tiptop msr-tools -y
+
