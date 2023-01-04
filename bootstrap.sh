9a81cfe1b6abb9d14f34e7c94323b1c06439b357
Juan Carlos Zamora <juazasan@msn.com>
2023-01-04 08:27:11 +0100

    vim customizations added

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,13 +26,48 @@ if ! curl "$URL" --silent --output "$HOME/.git-completion.bash"; then
 fi
 
 ### vim...
+# Pre-requisites
+scripts/devframeworks/install-go.sh
+scripts/devframeworks/install-node.sh
+
+# Download bundles
+mkdir profile/.vim/bundles -p
+pushd profile/.vim/bundles
+git clone https://github.com/ctrlpvim/ctrlp.vim.git ctrlp.vim
+git clone https://github.com/scrooloose/nerdtree.git nerdtree
+git clone https://github.com/majutsushi/tagbar.git tagbar
+git clone https://github.com/fatih/vim-go.git vim-go
+git clone https://github.com/jistr/vim-nerdtree-tabs.git vim-nerdtree-tabs
+git clone https://github.com/itchyny/lightline.vim lightline.vim
+git clone https://github.com/klen/python-mode.git python-mode
+pushd python-mode
+git submodule update --init --recursive
+popd
+git clone https://github.com/leafgarland/typescript-vim.git typescript-vim
+git clone https://github.com/tpope/vim-fugitive.git vim-fugitive
+git clone https://github.com/Valloric/YouCompleteMe.git YouCompleteMe
+pushd YouCompleteMe
+git submodule update --init --recursive
+source ~/.path.config
+python3 install.py --go-completer --ts-completer
+popd
+popd
+# Vim color scheme install
+# echo ''
+# echo "Now installing vim wombat color scheme..."
+# echo ''
+# git clone https://github.com/sheerun/vim-wombat-scheme.git ~/.vim/colors/wombat
+# mv ~/.vim/colors/wombat/colors/* ~/.vim/colors/
+
+# link .vimrc
+rm ~/.vimrc
+ln -s $(pwd)/profile/.vimrc ~/.vimrc
 
 ### configure profile
-
 # set tmux as default shell
 # chsh -s $(which tmux)
 
-# link dotfiles
+### link dotfiles
 touch ~/.path.config
 echo "source $(pwd)/profile/.bashrc" > ~/.bashrc
 ln -s $(pwd)/profile/.tmux.conf ~/.tmux.conf
